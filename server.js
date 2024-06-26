aa48d3221a0f56fc00ea8e7bd2d627abfd286f52
misaeltoa <23040025@alumno.utc.edu.mx>
2024-06-26 11:31:25 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Configurar middleware para servir archivos estáticos
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta para la página principal
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Iniciar el servidor
+
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en http://localhost:${PORT}`);
 });
